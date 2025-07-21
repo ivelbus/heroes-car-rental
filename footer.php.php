19eef887c1e78a5638ec53b9d5b8481f7a78826e
ivelbus <160024084+ivelbus@users.noreply.github.com>
2025-07-21 17:38:53 +0200

    Add files via upload

---
+++ footer.php.php
@@ -0,0 +1,11 @@
+<!-- Footer -->
+<footer class="footer">
+    <div class="container">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Logo Heroes Car Rental" class="logo-footer">
+        <p>© <?php echo date('Y'); ?> Heroes Car Rental. Todos los derechos reservados.</p>
+    </div>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
